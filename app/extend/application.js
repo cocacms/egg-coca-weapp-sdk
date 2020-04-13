b1101135d10c743cfa5aaedaa9cd97a799d27240
rojer <rojerchen@qq.com>
2020-04-13 12:00:42 +0800

    first commit

--- app/extend/application.js
+++ app/extend/application.js
@@ -10,12 +10,12 @@ module.exports = {
       this[WEAPPSDKS] = {};
     }
 
-    if (!this[WEAPPSDKMEMBER][key]) {
+    if (!this[WEAPPSDKS][key]) {
       const config = this.config.weapp[key];
       if (!config) {
         throw new Error("请先配置小程序sdk参数");
       }
-      this[WEAPPSDKMEMBER][key] = weappsdk(
+      this[WEAPPSDKS][key] = weappsdk(
         ...config,
         async () => {
           return await this.get_token(key);
@@ -26,12 +26,12 @@ module.exports = {
       );
     }
 
-    return this[WEAPPSDKMEMBER][key];
+    return this[WEAPPSDKS][key];
   },
 
   async get_token(key) {
     const token = await this.redis.get(`weappsdk.token.${key}`);
-    this.logger.debug("[weappsdk] get member token from redis:", token);
+    this.logger.debug("[weappsdk] get weappsdk token from redis:", token);
     if (!token) return {};
     try {
       const tokenobj = JSON.parse(token);
@@ -46,7 +46,7 @@ module.exports = {
     value.expires = dayjs()
       .add(7000, "second")
       .unix();
-    this.logger.debug("[weappsdk] set member token to redis:", value);
+    this.logger.debug("[weappsdk] set weappsdk token to redis:", value);
     await this.redis.set(`weappsdk.token.${key}`, JSON.stringify(value));
   }
 };
